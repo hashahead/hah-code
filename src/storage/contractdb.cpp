8b6a5b98d619a2c4fee5333462a1441565783c99
yaumfweds <hutseknmbwes@hotmail.com>
2026-01-19 22:08:00 +0800

    Add prune cleanup for contract KV roots and address root lookup

--- src/storage/contractdb.cpp
+++ src/storage/contractdb.cpp
@@ -119,6 +119,7 @@ bool CForkContractDB::RetrieveContractKvValue(const uint256& hashContractRoot, c
     ssKey.GetData(btKey);
     if (!dbTrie.Retrieve(hashContractRoot, btKey, btValue))
     {
+        StdLog("CForkContractDB", "Retrieve contract kv value: Retrieve failed, root: %s, key: %s", hashContractRoot.ToString().c_str(), key.ToString().c_str());
         return false;
     }
     try
@@ -134,117 +135,147 @@ bool CForkContractDB::RetrieveContractKvValue(const uint256& hashContractRoot, c
     return true;
 }
 
-//////////////////////////////
-// contract code
-
-bool CForkContractDB::AddCodeContext(const uint256& hashPrevBlock, const uint256& hashBlock,
-                                     const std::map<uint256, CContractSourceCodeContext>& mapSourceCode,
-                                     const std::map<uint256, CContractCreateCodeContext>& mapContractCreateCode,
-                                     const std::map<uint256, CContractRunCodeContext>& mapContractRunCode,
-                                     const std::map<uint256, CTemplateContext>& mapTemplateData,
-                                     uint256& hashCodeRoot)
+bool CForkContractDB::ClearContractKvRootUnavailableNode(const uint32 nRemoveLastHeight, bool& fExit)
 {
-    uint256 hashPrevRoot;
-    if (hashBlock != hashFork)
+    if (!fPruneData)
     {
-        if (!ReadTrieRoot(DB_CONTRACT_ROOT_TYPE_CODE, hashPrevBlock, hashPrevRoot))
+        StdLog("CForkContractDB", "Clear contract kv root unavailable node: Prune data is false, last height: %d, fork chainid: %d", nRemoveLastHeight, nChainId);
+        return false;
+    }
+
+    std::map<CDestination, std::map<uint256, std::pair<uint32, uint64>>> mapAddressRoot; // key1: contract address, key2: root, v1: height, v2: number
+    std::map<uint256, std::set<uint256>> mapRootNext;                                    // key: root, value: next root
+    if (!WalkThroughHeightRoot(mapAddressRoot, mapRootNext))
+    {
+        StdLog("CForkContractDB", "Clear contract kv root unavailable node: Walk through height root failed, last height: %d, fork chainid: %d", nRemoveLastHeight, nChainId);
+        return false;
+    }
+
+    std::map<uint64, std::map<uint256, CDestination>> mapRemoveRoot; // key1: block number, key2: root, value: address
+    for (const auto& kv : mapAddressRoot)
+    {
+        const CDestination& destAddress = kv.first;
+        std::map<uint64, std::set<uint256>> mapLastRootNumber; // key: block number, value: root
+        for (const auto& kv2 : kv.second)
         {
-            StdLog("CForkContractDB", "Add Code Context: Read trie root fail, hashPrevBlock: %s, hashBlock: %s",
-                   hashPrevBlock.GetHex().c_str(), hashBlock.GetHex().c_str());
-            return false;
+            const uint256& hashRoot = kv2.first;
+            const uint32 nBlockHeight = kv2.second.first;
+            const uint64 nBlockNumber = kv2.second.second;
+            if (nBlockHeight < nRemoveLastHeight)
+            {
+                auto it = mapRootNext.find(hashRoot);
+                if (it == mapRootNext.end() || it->second.empty())
+                {
+                    mapLastRootNumber[nBlockNumber].insert(hashRoot);
+                }
+                else
+                {
+                    mapRemoveRoot[nBlockNumber].insert(std::make_pair(hashRoot, destAddress));
+                }
+            }
+        }
+        if (mapLastRootNumber.size() > 1)
+        {
+            auto mt = mapLastRootNumber.rbegin();
+            ++mt;
+            for (; mt != mapLastRootNumber.rend(); ++mt)
+            {
+                const uint64 nBlockNumber = mt->first;
+                for (const auto& hashRoot : mt->second)
+                {
+                    mapRemoveRoot[nBlockNumber].insert(std::make_pair(hashRoot, destAddress));
+                }
+            }
         }
     }
 
-    bytesmap mapKv;
-    for (const auto& kv : mapSourceCode)
+    std::set<uint256> setNeedRemovedHash;
+    for (const auto& kv : mapRemoveRoot)
     {
-        hnbase::CBufStream ssKey, ssValue;
-        bytes btKey, btValue;
-
-        ssKey << DB_CONTRACT_KEY_TYPE_SOURCE_CODE << kv.first;
-        ssKey.GetData(btKey);
-
-        ssValue << kv.second;
-        ssValue.GetData(btValue);
-
-        mapKv.insert(make_pair(btKey, btValue));
-    }
-    for (const auto& kv : mapContractCreateCode)
-    {
-        hnbase::CBufStream ssKey, ssValue;
-        bytes btKey, btValue;
-
-        ssKey << DB_CONTRACT_KEY_TYPE_CONTRACT_CREATE_CODE << kv.first;
-        ssKey.GetData(btKey);
-
-        ssValue << kv.second;
-        ssValue.GetData(btValue);
-
-        mapKv.insert(make_pair(btKey, btValue));
-    }
-    for (const auto& kv : mapContractRunCode)
-    {
-        hnbase::CBufStream ssKey, ssValue;
-        bytes btKey, btValue;
-
-        ssKey << DB_CONTRACT_KEY_TYPE_CONTRACT_RUN_CODE << kv.first;
-        ssKey.GetData(btKey);
-
-        ssValue << kv.second;
-        ssValue.GetData(btValue);
-
-        mapKv.insert(make_pair(btKey, btValue));
-    }
-    for (const auto& kv : mapTemplateData)
-    {
-        hnbase::CBufStream ssKey, ssValue;
-        bytes btKey, btValue;
-
-        ssKey << DB_CONTRACT_KEY_TYPE_CONTRACT_TEMPLATE_DATA << kv.first;
-        ssKey.GetData(btKey);
-
-        ssValue << kv.second;
-        ssValue.GetData(btValue);
-
-        mapKv.insert(make_pair(btKey, btValue));
-    }
-    AddPrevRoot(DB_CONTRACT_ROOT_TYPE_CODE, hashPrevRoot, hashBlock, mapKv);
-
-    if (!dbTrie.AddNewTrie(hashPrevRoot, mapKv, hashCodeRoot))
-    {
-        StdLog("CForkContractDB", "Add Code Context: Add new trie fail, hashPrevBlock: %s, hashBlock: %s",
-               hashPrevBlock.GetHex().c_str(), hashBlock.GetHex().c_str());
-        return false;
+        for (const auto& kv2 : kv.second)
+        {
+            const uint256& hashRoot = kv2.first;
+            if (setNeedRemovedHash.count(hashRoot) == 0)
+            {
+                setNeedRemovedHash.insert(hashRoot);
+            }
+        }
     }
 
-    if (!WriteTrieRoot(DB_CONTRACT_ROOT_TYPE_CODE, hashBlock, hashCodeRoot))
+    StdDebug("CForkContractDB", "Clear contract kv root unavailable node: Remove root begin, total root count: %lu, last height: %d, fork chainid: %d",
+             setNeedRemovedHash.size(), nRemoveLastHeight, nChainId);
+
+    int64 t1 = GetTimeMillis();
+    int64 m1 = t1;
+
+    uint64 nTotalRemoveNodeCount = 0, nTotalLinkerCount = 0;
+    std::set<uint256> setRemovedHash;
+    for (const auto& kv : mapRemoveRoot)
     {
-        StdLog("CForkContractDB", "Add Code Context: Write trie root fail, hashPrevBlock: %s, hashBlock: %s",
-               hashPrevBlock.GetHex().c_str(), hashBlock.GetHex().c_str());
-        return false;
+        const uint64 nBlockNumber = kv.first;
+        for (const auto& kv2 : kv.second)
+        {
+            const uint256& hashRoot = kv2.first;
+            const CDestination& destAddress = kv2.second;
+            if (setRemovedHash.count(hashRoot) == 0)
+            {
+                setRemovedHash.insert(hashRoot);
+
+                if (setRemovedHash.size() % 100 == 0)
+                {
+                    int64 m2 = GetTimeMillis();
+                    int64 sm = m2 - m1;
+                    m1 = m2;
+                    StdDebug("CForkContractDB", "Clear contract kv root unavailable node: Remove part success, elapsed time: %lu ms (%lu ms), average: %.3f ms/root, remove node count: %lu, remove linker count: %lu, remove root count: %lu, total root count: %lu, block number: %lu, last height: %d, fork chainid: %d",
+                             sm, m2 - t1, (double)sm / 100, nTotalRemoveNodeCount, nTotalLinkerCount, setRemovedHash.size(), setNeedRemovedHash.size(), nBlockNumber, nRemoveLastHeight, nChainId);
+                }
+            }
+            if (!RemoveAddressRoot(destAddress, hashRoot))
+            {
+                StdLog("CForkContractDB", "Clear contract kv root unavailable node: Remove address root failed, address: %s, root: %s, block number: %lu, last height: %d, fork chainid: %d",
+                       destAddress.ToString().c_str(), hashRoot.ToString().c_str(), nBlockNumber, nRemoveLastHeight, nChainId);
+                return false;
+            }
+            if (fExit)
+            {
+                break;
+            }
+        }
+        if (fExit)
+        {
+            break;
+        }
     }
+
+    int64 t2 = GetTimeMillis();
+    int64 et = t2 - t1;
+    double dPerBlockTime = 0.0;
+    double dPerRootTime = 0.0;
+    if (mapRemoveRoot.size() > 0 && et > 0)
+    {
+        dPerBlockTime = (double)et / mapRemoveRoot.size();
+    }
+    if (setRemovedHash.size() > 0 && et > 0)
+    {
+        dPerRootTime = (double)et / setRemovedHash.size();
+    }
+
+    StdDebug("CForkContractDB", "Clear contract kv root unavailable node: Remove success, elapsed time: %lu ms, per block time: %.2f ms, per root time: %.2f ms, remove node count: %lu, remove linker count: %lu, remove root count: %lu, block count: %lu, last height: %d, fork chainid: %d",
+             et, dPerBlockTime, dPerRootTime, nTotalRemoveNodeCount, nTotalLinkerCount, setRemovedHash.size(), mapRemoveRoot.size(), nRemoveLastHeight, nChainId);
     return true;
 }
 
-bool CForkContractDB::RetrieveSourceCodeContext(const uint256& hashBlock, const uint256& hashSourceCode, CContractSourceCodeContext& ctxtCode)
+bool CForkContractDB::GetContractAddressRoot(const CDestination& destContract, const uint256& hashRoot, uint256& hashPrevRoot, uint32& nBlockHeight, uint64& nBlockNumber)
 {
-    uint256 hashCodeRoot;
-    if (!ReadTrieRoot(DB_CONTRACT_ROOT_TYPE_CODE, hashBlock, hashCodeRoot))
-    {
-        return false;
-    }
     hnbase::CBufStream ssKey, ssValue;
-    bytes btKey, btValue;
-    ssKey << DB_CONTRACT_KEY_TYPE_SOURCE_CODE << hashSourceCode;
-    ssKey.GetData(btKey);
-    if (!dbTrie.Retrieve(hashCodeRoot, btKey, btValue))
+    ssKey << DB_CONTRACT_KEY_TYPE_ADDRESS_ROOT << destContract << hashRoot;
+    if (!dbTrie.ReadExtKv(ssKey, ssValue))
     {
         return false;
     }
     try
     {
-        ssValue.Write((char*)(btValue.data()), btValue.size());
-        ssValue >> ctxtCode;
+        ssValue >> hashPrevRoot >> nBlockHeight >> nBlockNumber;
     }
     catch (std::exception& e)
     {
