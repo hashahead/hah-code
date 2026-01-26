ef70d4c58f06aaa1a3ccd747fb0347d3c239baae
tomnsbt35 <tomnsbt35@outlook.com>
2026-01-26 13:27:33 +0800

    prunedb: add prune database module

---
+++ src/blockchain/prunedb.cpp
@@ -0,0 +1,57 @@
+// Copyright (c) 2021-2025 The HashAhead developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "prunedb.h"
+
+#include "block.h"
+
+using namespace hnbase;
+
+namespace hashahead
+{
+
+/////////////////////////////////////////////////
+// CPruneDb
+
+CPruneDb::CPruneDb()
+{
+    pCoreProtocol = nullptr;
+    pBlockChain = nullptr;
+}
+
+CPruneDb::~CPruneDb()
+{
+}
+
+bool CPruneDb::HandleInitialize()
+{
+    if (!GetObject("coreprotocol", pCoreProtocol))
+    {
+        Error("Failed to request coreprotocol");
+        return false;
+    }
+    if (!GetObject("blockchain", pBlockChain))
+    {
+        Error("Failed to request blockchain");
+        return false;
+    }
+    return true;
+}
+
+void CPruneDb::HandleDeinitialize()
+{
+    pCoreProtocol = nullptr;
+    pBlockChain = nullptr;
+}
+
+bool CPruneDb::HandleInvoke()
+{
+    return true;
+}
+
+void CPruneDb::HandleHalt()
+{
+}
+
+} // namespace hashahead
---
+++ src/blockchain/prunedb.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2021-2025 The HashAhead developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef HASHAHEAD_PRUNEDB_H
+#define HASHAHEAD_PRUNEDB_H
+
+#include "base.h"
+
+namespace hashahead
+{
+
+class CPruneDb : public IPruneDb
+{
+public:
+    CPruneDb();
+    ~CPruneDb();
+
+protected:
+    bool HandleInitialize() override;
+    void HandleDeinitialize() override;
+    bool HandleInvoke() override;
+    void HandleHalt() override;
+
+protected:
+    ICoreProtocol* pCoreProtocol;
+    IBlockChain* pBlockChain;
+};
+
+} // namespace hashahead
+
+#endif // HASHAHEAD_PRUNEDB_H
